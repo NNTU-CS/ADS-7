3bde61342ec773782ce854496a73ed1059afb20a
AlexanderSofin <99262399+AlexanderSofin@users.noreply.github.com>
2022-04-30 14:52:54 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -8,30 +8,28 @@ class TPQueue {
         T value;
         ITEM* next;
     };
-public:
+ public:
     TPQueue() :head(nullptr), tail(nullptr) {}
     ~TPQueue();
     void push(const T&);
     T pop();
     void print() const;
-private:
+ private:
     TPQueue::ITEM* create(const T&);
     ITEM* head;
     ITEM* dop;
     ITEM* tail;
-};  
+};
   // реализация шаблона очереди с приоритетом на связанном списке
 template<typename T>
-typename TPQueue<T>::ITEM* TPQueue<T>::create(const T& value)
-{
+typename TPQueue<T>::ITEM* TPQueue<T>::create(const T& value) {
     ITEM* item = new ITEM;
     item->value = value;
     item->next = nullptr;
     return item;
 }
 template<typename T>
-TPQueue<T>::~TPQueue()
-{
+TPQueue<T>::~TPQueue() {
     while (head)
         pop();
 }
@@ -42,15 +40,16 @@ void TPQueue<T>::push(const T& inf) {
         dop = head;
         tail = head;
     }
-    else if (tail->value.prior >= inf.prior) {
-        if (tail->value.prior == inf.prior && tail->value.ch == inf.ch) { 
-            tail->value = inf;
+    else {
+        if (tail->value.prior >= inf.prior) {
+            if (tail->value.prior == inf.prior && tail->value.ch == inf.ch)
+                tail->value = inf;
         }
-        else {
-            if (tail->value.prior >= inf.prior && tail->value.ch != inf.ch) {
-                tail->next = create(inf);
-                tail = tail->next;
-            }
+    }
+    else {
+        if (tail->value.prior >= inf.prior && tail->value.ch != inf.ch) {
+            tail->next = create(inf);
+            tail = tail->next;
         }
     }
     else {
@@ -61,25 +60,27 @@ void TPQueue<T>::push(const T& inf) {
                 tmp->next = head;
                 head = tmp;
             }
-            else 
-               if (inf.prior == head->value.prior) {
-                 if (inf.ch == head->value.ch)
+        }
+    }
+    else {
+        if (inf.prior == head->value.prior) {
+            if (inf.ch == head->value.ch) {
                    head->value = inf;
-               }
-            else {
-               ITEM* dop = nullptr;
-               dop = create(inf);
-               dop->next = head->next;
-               head->next = dop;
             }
-            else {
-               if (inf.prior < head->value.prior) {
-                 ITEM* dop = nullptr;
-                 dop = create(inf);
-                 dop->next = head->next;
-                 head->next = dop;
-               }
-            }
+        }
+    }
+    else {
+        ITEM* dop = nullptr;
+        dop = create(inf);
+        dop->next = head->next;
+        head->next = dop;
+    }
+    else {
+        if (inf.prior < head->value.prior) {
+            ITEM* dop = nullptr;
+            dop = create(inf);
+            dop->next = head->next;
+            head->next = dop;
         }
     }
 }
