66ad3549ab0b126db36760ed32f326e95b78b22b
TsvetyaevSergey <99178429+TsvetyaevSergey@users.noreply.github.com>
2022-05-03 12:17:28 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,10 +2,6 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-// Copyright 2022 NNTU-CS
-#ifndef INCLUDE_TPQUEUE_H_
-#define INCLUDE_TPQUEUE_H_
-
 template<typename T>
 class TPQueue {
  private:
