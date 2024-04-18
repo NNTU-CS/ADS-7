2e801702084f114720f80ad9db15db0161bc6ffd
Tsvetov1984 <acvetov68@gmail.com>
2024-04-18 21:13:13 +0300

    ASD-7

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,14 +2,70 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T>
-class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
-};
+#include <string>
 
 struct SYM {
-  char ch;
-  int prior;
+    char ch;
+    int prior;
+};
+
+template<typename T>
+class TPQueue {
+ private:
+    struct Slist {
+        Slist *next;
+        T current;
+    };
+    Slist *prev;
+    Slist *end;
+
+    Slist *create(const T &item) {
+        Slist *templ = new Slist;
+        templ->current = item;
+        templ->next = nullptr;
+        return templ;
+    }
+
+ public:
+    TPQueue() : prev(nullptr), end(nullptr) {}
+
+    void push(const T &item) {
+        Slist *templ = create(item);
+        if (prev == nullptr) {
+            end = templ;
+            prev = templ;
+        } else {
+            if (item.prior > prev->current.prior) {
+                templ->next = prev;
+                prev = templ;
+            } else {
+                Slist *hoop = prev;
+                while (item.prior <= hoop->next->current.prior && hoop->next != nullptr)
+                    hoop = hoop->next;
+                templ = hoop->next;
+                hoop->next = templ;
+                if (templ->next == nullptr)
+                    end = templ;
+            }
+        }
+    }
+
+    const T pop() {
+        if (isEmpty()) {
+            std::cout << "empty!" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+        Slist *temp = prev;
+        T result = temp->current;
+        prev = prev->next;
+        delete temp;
+        return result;
+    }
+
+    bool isEmpty() const {
+        return prev == nullptr;
+    }
+
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
