cfc4578ffcff0fa56f4c9e18222997dbbbbcd7b8
Frollluuu <Frolovaannet2004@yandex.ru>
2024-04-19 16:41:53 +0300

    7:(

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -6,49 +6,58 @@
 
 template <typename T>
 class TPQueue {
-    private:
-        struct Node {
-            T data;
-            Node* next;
-        };
-        Node* head;
-        Node* tail;
+ private:
+    struct Node {
+        T data;
+        Node* next;
+    };
 
-    public:
-        TPQueue() : head(nullptr), tail(nullptr) {}
-        void push(const T& data) {
-            Node* newNode = new Node{data, nullptr};
-            if (!head || head->data.prior < data.prior) {
-                newNode->next = head;
-                head = newNode;
-            } else {
-                Node* current = head;
-                while (current->next && current->next->data.prior >= data.prior) {
-                    current = current->next;
-                }
-                newNode->next = current->next;
-                current->next = newNode;
+    Node* head;
+    Node* tail;
+
+ public:
+    TPQueue() : head(nullptr), tail(nullptr) {}
+
+    void push(const T& data) {
+        Node* newNode = new Node{data, nullptr};
+
+        if (!head || head->data.prior < data.prior) {
+            newNode->next = head;
+            head = newNode;
+        } else {
+            Node* current = head;
+            while (current->next && current->next->data.prior >= data.prior) {
+                current = current->next;
             }
-            if (!tail) {
-                tail = head;
-            }
+            newNode->next = current->next;
+            current->next = newNode;
         }
-        T pop() {
-            if (!head) {
-                throw std::out_of_range("Queue is empty");
-            }
-            T data = head->data;
-            Node* temp = head;
-            head = head->next;
-            delete temp;
-            if (!head) {
-                tail = nullptr;
-            }
-            return data;
+
+        if (!tail) {
+            tail = head;
         }
-        bool isEmpty() const {
-            return !head;
+    }
+
+    T pop() {
+        if (!head) {
+            throw std::out_of_range("Queue is empty");
         }
+
+        T data = head->data;
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+
+        if (!head) {
+            tail = nullptr;
+        }
+
+        return data;
+    }
+
+    bool isEmpty() const {
+        return !head;
+    }
 };
 
 struct SYM {
