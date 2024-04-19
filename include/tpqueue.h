e0543ed250c10ece14b2bad37f98d9911fe1d888
Frollluuu <Frolovaannet2004@yandex.ru>
2024-04-19 16:35:37 +0300

    5

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -7,18 +7,14 @@
 template <typename T>
 class TPQueue {
     private:
-
         struct Node {
             T data;
             Node* next;
         };
-
         Node* head;
         Node* tail;
     public:
-
         TPQueue() : head(nullptr), tail(nullptr) {}
-
         void push(const T& data) {
             Node* newNode = new Node{data, nullptr};
 
@@ -33,29 +29,23 @@ class TPQueue {
                 newNode->next = current->next;
                 current->next = newNode;
             }
-
             if (!tail) {
                 tail = head;
             }
         }
-
         T pop() {
             if (!head) {
                 throw std::out_of_range("Queue is empty");
             }
-
             T data = head->data;
             Node* temp = head;
             head = head->next;
             delete temp;
-
             if (!head) {
                 tail = nullptr;
             }
-
             return data;
         }
-
         bool isEmpty() const {
             return !head;
         }
