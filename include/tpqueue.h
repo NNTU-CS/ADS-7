615a5160d0fa258345d86ffb822a9aa0163a87b5
Arina-Arahis <159996104+Arina-Arahis@users.noreply.github.com>
2024-04-22 23:23:32 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,82 +1,81 @@
-// Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
 template<typename T>
 class TPQueue {
   // реализация шаблона очереди с приоритетом на связанном списке
-  struct Node {
-    T item;
-    Node* next = nullptr;
-    Node* prev = nullptr;
+  struct Item {
+    T data;
+    Item* next = nullptr;
+    Item* prev = nullptr;
   };
-  Node* create(const T& item) {
-    Node* node = new Node;
-    node->item = item;
-    return node;
+  Item* create(const T& data) {
+    Item* item = new Item;
+    item->data = data;
+    return item;
   }
   bool IsEmpty() {
-    return head == nullptr;
+    return start == nullptr;
   }
-  Node* head;
-  Node* tail;
-  Node* current;
+  Item* start;
+  Item* finish;
+  Item* temp;
 
  public:
   TPQueue() {
-    head = tail = current = nullptr;
+    start = finish = temp = nullptr;
   }
   ~TPQueue() {
     if (IsEmpty()) {
       return;
-    } else if (head->next) {
-      current = head->next;
-      while (current != tail) {
-        delete current->prev;
-        current = current->next;
+    } else if (start->next) {
+      temp = start->next;
+      while (temp != finish) {
+        delete temp->prev;
+        temp = temp->next;
       }
-      delete tail;
+      delete finish;
     } else {
-      delete head;
+      delete start;
     }
   }
-  void push(T item) {
+  void push(T sym) {
     if (IsEmpty()) {
-      head = tail = create(item);
+      start = finish = create(sym);
     } else {
-      Node* temp = create(item);
-      current = head;
-      if (item.prior <= head->item.prior && item.prior > tail->item.prior) {
-        while (item.prior <= current->item.prior) {
-          current = current->next;
+      Item* temp_item = create(sym);
+      temp = start;
+      if (sym.prior <= start->data.prior && sym.prior > finish->data.prior) {
+        while (sym.prior <= temp->data.prior) {
+          temp = temp->next;
         }
-        current = current->prev;
-        temp->prev = current;
-        temp->next = current->next;
-        current->next->prev = temp;
-        current->next = temp;
-      } else if (item.prior > head->item.prior) {
-        temp->next = head;
-        head->prev = temp;
-        head = temp;
+        temp = temp->prev;
+        temp_item->prev = temp;
+        temp_item->next = temp->next;
+        temp->next->prev = temp_item;
+        temp->next = temp_item;
+      } else if (sym.prior > start->data.prior) {
+        temp_item->next = start;
+        start->prev = temp_item;
+        start = temp_item;
       } else {
-        tail->next = temp;
-        tail->next->prev = tail;
-        tail = tail->next;
+        finish->next = temp_item;
+        finish->next->prev = finish;
+        finish = finish->next;
       }
     }
   }
   T pop() {
-    if (head && tail) {
-      Node* temp = head->next;
-      if (head->next != nullptr) {
+    if (start && finish) {
+      temp = start->next;
+      if (start->next != nullptr) {
         temp->prev = nullptr;
       }
-      T data = head->item;
-      delete head;
-      head = temp;
-      if (head == nullptr)
-        tail = nullptr;
+      T data = start->data;
+      delete start;
+      start = temp;
+      if (start == nullptr)
+        finish = nullptr;
       return data;
     } else {
       T fake{'0', -10};
@@ -85,7 +84,7 @@ class TPQueue {
   }
 };
 
-struct Symbol {
+struct SYM {
   char ch;
   int prior;
 };
