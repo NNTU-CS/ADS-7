c247b8e8eb3682f09826223c6d1439509fcdcce4
fakemove <160417424+fakemove@users.noreply.github.com>
2024-04-23 17:03:48 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,50 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+private:
+    struct Node {
+        T data;
+        Node* next;
+    };
+    Node* begin;
+    Node* end;
+
+ public:
+    TPQueue() : begin(nullptr), end(nullptr) {}
+
+    void push(const T& data) {
+      Node* new_mode = new Node{data, nullptr};
+      if (!begin || begin->data.prior < data.prior) {
+        new_mode->next = begin;
+        begin = new_mode;
+      } else {
+        Node* current = begin;
+        while (current->next &&  current->next->data.prior >= data.prior) {
+          current = current->next;
+        }
+        new_mode->next = current->next;
+        current->next = new_mode;
+      }
+      if (!end) {
+        end = begin;
+      }
+    }
+    T pop() {
+      if (!begin) {
+        throw std::out_of_range("Queue empty");
+      }
+      T data = begin->data;
+      Node* temp = begin;
+      begin = begin->next;
+      delete temp;
+      if (!begin) {
+        end = nullptr;
+      }
+      return data;
+    }
+    bool is_empty() {
+      return !begin;
+    }
 };
 
 struct SYM {
