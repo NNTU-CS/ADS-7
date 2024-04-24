d464d9129691f3ed554319efe5d265c28da2e109
FomenkovaAnastasia <160059344+FomenkovaAnastasia@users.noreply.github.com>
2024-04-24 13:29:03 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,12 +1,44 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
-
+#include <iostream>
+#include <stdexcept>
+#include <string>
 template<typename T>
-class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
-};
+class TPQueue{
+ private:
+    Node* prev;
+ public:
+    TPQueue() : prev(nullptr) {}
+    void push(const SYM& data) {
+        Node* newNode = new Node;
+        newNode->data = data;
+        if (prev == nullptr || prev->data.prior < data.prior) {
+            newNode->next = prev;
+            prev = newNode;
+        }
+        else {
+            Node* current = prev;
+            while (current->next != nullptr && current->next->data.prior >= data.prior) {
+                current = current->next;
+            }
+            newNode->next = current->next;
+            current->next = newNode;
+        }
+    }
 
+    SYM pop() {
+        if (prev == nullptr) {
+            throw std::string("Queue is empty");
+            return SYM();
+        }
+        Node* temp = prev;
+        prev = prev->next;
+        SYM data = temp->data;
+        delete temp;
+        return data;
+    }
+};
 struct SYM {
   char ch;
   int prior;
