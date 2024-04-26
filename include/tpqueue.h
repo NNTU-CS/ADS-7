bcdd75796826bbd5380808275037367be35c188d
UlianaKustova <91365332+UlianaKustova@users.noreply.github.com>
2024-04-26 13:20:08 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,14 +1,15 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <string>
 
 template<typename T>
 class TPQueue {
-   struct ITEM {
-       T data;
-       ITEM* next;
-   };
-private:
+    struct ITEM {
+        T data;
+        ITEM* next;
+    };
+ private:
     //TPQueue::ITEM* create(const T&);
     ITEM* head;
     ITEM* tail;
@@ -18,7 +19,7 @@ private:
         item->next = nullptr;
         return item;
     }
-public:
+ public:
     TPQueue() {
         head = nullptr;
         tail = nullptr;
