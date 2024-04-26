4a128f92f1dc26143d2a27f048a4aa8376a2ccf8
Dobro <76173176+OlesuaDobro@users.noreply.github.com>
2024-04-26 16:06:15 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,9 +4,84 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+struct SYM {
+  char ch;
+  int prior;
 };
-
+struct Node {
+  SYM data;
+  Node* next;
+};
+template<typename T>
+class TPQueue {
+private:
+  Node* head;
+  Node* tail;
+public:
+  TPQueue() : head(nullptr), tail(nullptr) {}
+  ~TPQueue() { clear(); }
+  void push(const T& item) {
+    Node* newNode = new Node;
+    newNode->data = item;
+    newNode->next = nullptr;
+    if (isEmpty()) {
+      head = newNode;
+      tail = newNode;
+    } else {
+      Node* current = head;
+      Node* prev = nullptr;
+      while (current != nullptr && current->data.prior >= item.prior) {
+        prev = current;
+        current = current->next;
+      }
+      if (prev == nullptr) {
+        newNode->next = head;
+        head = newNode;
+      } else {
+        prev->next = newNode;
+        newNode->next = current;
+      }
+    }
+  }
+  T pop() {
+    if (isEmpty()) {
+      throw std::out_of_range("Queue is empty");
+    }
+    T front = head->data;
+    Node* temp = head;
+    head = head->next;
+    delete temp;
+    if (head == nullptr) {
+      tail = nullptr;
+    }
+    return front;
+  }
+  bool isEmpty() const {
+    return head == nullptr;
+  }
+  void clear() {
+    while (!isEmpty()) {
+      pop();
+    }
+  }
+};
+int main() {
+  TPQueue<SYM> queue;
+  SYM item1 = {'A', 3};
+  queue.push(item1);
+  SYM item2 = {'B', 2};
+  queue.push(item2);
+  SYM item3 = {'C', 1};
+  queue.push(item3);
+  SYM front = queue.pop();
+  std::cout << "Front: " << front.ch << ", Priority: " << front.prior << std::endl;
+  front = queue.pop();
+  std::cout << "Front: " << front.ch << ", Priority: " << front.prior << std::endl;
+  front = queue.pop();
+  std::cout << "Front: " << front.ch << ", Priority: " << front.prior << std::endl;
+  return 0;
+}
+};
 struct SYM {
   char ch;
   int prior;
