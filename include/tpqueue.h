dc3c3ee5f720169c9d24591c58d487f04e901ce2
AnastasiaKotina <160050075+AnastasiaKotina@users.noreply.github.com>
2024-04-26 22:51:08 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,14 +2,53 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T>
-class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+struct SYM {
+    char ch;
+    int prior;
 };
 
-struct SYM {
-  char ch;
-  int prior;
+template<typename T>
+class TPQueue {
+  private:
+    struct Node {
+        SYM data;
+        Node* next;
+    };
+    Node* head;
+
+public:
+    TPQueue() : head(nullptr) {}
+
+    void push(SYM sym) {
+        Node* new_node = new Node{ sym, nullptr };
+
+        if (head == nullptr || sym.prior > head->data.prior) {
+            new_node->next = head;
+            head = new_node;
+        }
+        else {
+            Node* temp = head;
+            while (temp->next != nullptr && sym.prior <= temp->next->data.prior) {
+                temp = temp->next;
+            }
+            new_node->next = temp->next;
+            temp->next = new_node;
+        }
+    }
+
+    SYM pop() {
+        if (head == nullptr) {
+            std::cerr << "Queue is empty" << std::endl;
+            return SYM{ '0', 0 };
+        }
+
+        SYM sym = head->data;
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+
+        return sym;
+    }
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
