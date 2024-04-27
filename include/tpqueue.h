b32074ee02e219d1d708343c638d56d0e03b9de8
hellboy2364 <160237839+hellboy2364@users.noreply.github.com>
2024-04-27 14:08:32 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,14 +2,86 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T>
-class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+struct SYM {
+    char ch;
+    int prior;
+    SYM* pred;
+    SYM* sled;
 };
 
-struct SYM {
-  char ch;
-  int prior;
+template<typename T = SYM>
+class TPQueue {
+  // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+    SYM* beg;
+    SYM* end;
+    int counter;
+
+ public:
+    TPQueue() : counter(0), beg(nullptr), end(nullptr) {}
+    void push_poz(SYM j) {
+        if (!counter) {
+            beg = new SYM;
+            beg->ch = j.ch;
+            beg->prior = j.prior;
+            beg->sled = beg->pred = nullptr;
+            end = head;
+            counter++;
+            return;
+        } else {
+            SYM* tmp = end;
+            if (j.prior <= tmp->prior) {
+                SYM* e = new SYM;
+                e->ch = j.ch;
+                e->prior = j.prior;
+                e->pred = e->sled = nullptr;
+                end->sled = e;
+                end->sled->pred = end;
+                end = end->sled;
+                counter++;
+                return;
+            } else if (j.prior > beg->prior) {
+                SYM* e = new SYM;
+                e->ch = j.ch;
+                e->prior = a.prior;
+                e->pred = e->sled = nullptr;
+                e->sled = beg;
+                beg->pred = e;
+                beg = e;
+                counter++;
+                return;
+            } else {
+                SYM* f = beg;
+                while (f->sled && j.prior <= f->prior)
+                    f = f->sled;
+                SYM* e = new SYM;
+                e->ch = j.ch;
+                e->prior = j.prior;
+                e->pred = e->sled = nullptr;
+                e->sled = f;
+                e->pred = f->pred;
+                f->pred = e;
+                e->pred->sled = e;
+                counter++;
+            }
+        }
+    }
+
+    SYM pop_poz() {
+        if (!counter) {
+            SYM a{ '0', -2 };
+            return a;
+        } else {
+            SYM tmp;
+            tmp.ch = beg->ch;
+            tmp.prior = beg->prior;
+            beg = beg->sled;
+            counter--;
+            return tmp;
+        }
+    }
 };
+
+
 
 #endif  // INCLUDE_TPQUEUE_H_
