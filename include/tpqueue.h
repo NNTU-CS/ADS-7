5d5cf696289fde7d91ecb84f361367750000c5d3
LisaKil <96301882+LisaKil@users.noreply.github.com>
2024-04-27 17:48:49 +0300

    Update tpqueue.h aaaa1

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,10 +1,53 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <stdexcept>
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+  private:
+    struct Item {
+        T data;
+        Item* next;
+    };
+    Item* head;
+    Item* tail;
+
+  public:
+    TPQueue() : head(nullptr), tail(nullptr) {}
+    void push (const T& data) {
+      Item* new_mode = new Item{data, nullptr};
+      if (!head || head->data.prior < data.prior) {
+        new_mode->next = head;
+        head = new_mode;
+      } else {
+        Item* current = head;
+        while (current->next &&  current->next->data.prior >= data.prior) {
+          current = current->next;
+        }
+        new_mode->next =current->next;
+        current->next = new_mode;
+      }
+      if (!tail) {
+        tail = head;
+      }
+    }
+    T pop() {
+      if (!head) {
+        throw std::out_of_range("Empty!");
+      }
+      T data = head->data;
+      Item* temp = head;
+      head = head->next;
+      delete temp;
+      if (!head) {
+        tail = nullptr;
+      }
+      return data;
+    }
+    bool is_empty() {
+      return !head;
+    }
 };
 
 struct SYM {
