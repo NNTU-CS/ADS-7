7e976f7ac9e76fd442de6919b58d9934251c38bd
Lizochek-Neko <160259786+Lizochek-Neko@users.noreply.github.com>
2024-04-27 14:15:23 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,9 +2,94 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
+#include <string>
+
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+    int first;
+    int last;
+    struct ITEM {
+        T data;
+        ITEM* next;
+    };
+    ITEM* head;
+    ITEM* tail;
+ public:
+    TPQueue() :first(0), last(0), head(nullptr), tail(nullptr) {}
+    void push(T x) {
+        if (last - first >= size)
+            throw std::string("Full!");
+        ITEM* item = head;
+        ITEM* prev = nullptr;
+        while (item) {
+            T s = item->data;
+            if (x.prior > s.prior) {
+                break;
+            }
+            prev = item;
+            item = item->next;
+        }
+        if (!item) {
+            addTail(x);
+        } else {
+            if (prev) {
+                insertAfter(prev, x);
+            } else {
+                addHead(x);
+            }
+        }
+        last++;
+    }
+    T pop() {
+        first++;
+        return rmHead();
+    }
+    int getSize() {
+        return (last - first);
+    }
+ private:
+    ITEM* create(const T& data) {
+        ITEM* item = new ITEM;
+        item->data = data;
+        item->next = nullptr;
+        return item;
+    }
+    void addTail(const T& data) {
+        if (tail && head) {
+            tail->next = create(data);
+            tail = tail->next;
+        } else {
+            head = create(data);
+            tail = head;
+        }
+    }
+    void addHead(const T& data) {
+        if (tail && head) {
+            ITEM* temp = create(data);
+            temp->next = head;
+            head = temp;
+        } else {
+            head = create(data);
+            tail = head;
+        }
+    }
+    void insertAfter(ITEM* prev, const T& data) {
+        ITEM* next = prev->next;
+        ITEM* new_item = create(data);
+        prev->next = new_item;
+        new_item->next = next;
+    }
+    T rmHead() {
+        if (head) {
+            ITEM* temp = head->next;
+            T data = head->data;
+            delete head;
+            head = temp;
+            return data;
+        }
+        throw std::string("Empty queue");
+    }
 };
 
 struct SYM {
