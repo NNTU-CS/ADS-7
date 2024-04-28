843a58ee5fcea1d2e3be0091fbfc6e978fa75ee8
flapik19 <selivanovvlad3211@gmail.com>
2024-04-28 14:36:57 +0300

    ads 7

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,9 +2,73 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
+#include <iterator>
+#include <stdexcept>
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+private:
+    struct Node {
+        T data;
+        Node* next;
+        int prior;
+    };
+
+    Node* head;
+    Node* tail;
+
+public:
+    TPQueue() : head(nullptr), tail(nullptr) {}
+
+    void push(const T& data, int prior) {
+        Node* newNode = new Node;
+        newNode->data = data;
+        newNode->prior = prior;
+        newNode->next = nullptr;
+
+        if (!head) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            if (prior > head->prior) {
+                newNode->next = head;
+                head = newNode;
+            } else {
+                Node* current = head;
+                while (current->next && prior <= current->next->prior) {
+                    current = current->next;
+                }
+                newNode->next = current->next;
+                current->next = newNode;
+                if (!newNode->next) {
+                    tail = newNode;
+                }
+            }
+        }
+    }
+
+    T pop() {
+        if (!head) {
+           throw std::out_of_range (" ");
+        }
+
+        T data = head->data;
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+        return data;
+    }
+
+    bool isEmpty() {
+        return !head;
+    }
+
+    ~TPQueue() {
+        while (head) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
 };
 
 struct SYM {
