b000f9b0725fc0703be3c70937cecb2fb3f95410
momchik5 <160311115+momchik5@users.noreply.github.com>
2024-04-28 23:42:21 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,15 +1,54 @@
 // Copyright 2022 NNTU-CS
+#include <string>
+
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T>
-class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+struct SYM {
+    char ch;
+    int prior;
 };
 
-struct SYM {
-  char ch;
-  int prior;
+struct sys {
+    SYM sym;
+    sys* next;
+};
+
+template<typename T = SYM>
+class TPQueue {
+ private:
+    sys* ptr;
+
+ public:
+    TPQueue() : ptr(nullptr) {}
+
+    void push(T newsym) {
+        sys* list = new Qlist;
+        list->sym = newsym;
+        list->next = nullptr;
+
+        if (ptr == nullptr || \
+            newsym.prior > ptr->sym.prior) {
+            list->next = ptr;
+            ptr = list;
+        } else {
+            sys* tmp = ptr;
+            while (tmp->next != nullptr && \
+                tmp->next->sym.prior > newsym.prior) {
+                tmp = tmp->next;
+            }
+            list->next = tmp->next;
+            tmp->next = list;
+        }
+    }
+
+    T pop() {
+        T reset = ptr->sym;
+        sys* tempor = ptr;
+        ptr = ptr->next;
+        delete tempor;
+        return reset;
+    }
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
