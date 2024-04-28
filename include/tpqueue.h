daf026af9f31d4eafd7fcf7ac91e69569d0a8be7
MariaSadkova <160345524+MariaSadkova@users.noreply.github.com>
2024-04-28 21:21:29 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,9 +2,60 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T>
+#include <iostream>
+
+struct Node {
+    SYM data;
+    Node* next;
+};
+
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+private:
+    Node* head;
+public:
+    TPQueue() {
+        head = nullptr;
+    }
+
+    void push(SYM item) {
+        Node* newNode = new Node;
+        newNode->data = item;
+        
+        if (!head || head->data.prior < item.prior) {
+            newNode->next = head;
+            head = newNode;
+        } else {
+            Node* current = head;
+            while (current->next && current->next->data.prior >= item.prior) {
+                current = current->next;
+            }
+            newNode->next = current->next;
+            current->next = newNode;
+        }
+    }
+
+    SYM pop() {
+        if (!head) {
+            throw std::out_of_range("Queue is empty");
+        }
+        Node* temp = head;
+        SYM item = head->data;
+        head = head->next;
+        delete temp;
+        return item;
+    }
+
+    bool isEmpty() {
+        return head == nullptr;
+    }
+
+    void print() {
+        Node* current = head;
+        while (current) {
+            std::cout << "Character: " << current->data.ch << ", Priority: " << current->data.prior << std::endl;
+            current = current->next;
+        }
+    }
 };
 
 struct SYM {
