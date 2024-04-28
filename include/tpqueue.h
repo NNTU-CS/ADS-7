cbc8974b1a5930f5ae3ae7a61746c028b4b9e97a
DisneyLib <160058897+TimoshchenkoElizaveta@users.noreply.github.com>
2024-04-28 12:52:16 +0300

    B67

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,39 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+    struct node {
+        SYM data;
+        node* next;
+    };
+    node* head;
+ public:
+    TPQueue() : head(nullptr) {}
+    void push(SYM new_sym) {
+        node* new_node = new node{ new_sym, nullptr };
+        if (head == nullptr || new_sym.prior > head->data.prior) {
+            new_node->next = head;
+            head = new_node;
+        } else {
+            node* tmp = head;
+            while (tmp->next != nullptr &&
+                new_sym.prior <= tmp->next->data.prior) {
+                tmp = tmp->next;
+            }
+            new_node->next = tmp->next;
+            tmp->next = new_node;
+          }
+    }
+    SYM pop() {
+        if (head == nullptr) {
+            return SYM{ '0', 0 };
+        }
+        SYM last = head->data;
+        node* tmp = head;
+        head = head->next;
+        delete tmp;
+        return last;
+    }
 };
 
 struct SYM {
