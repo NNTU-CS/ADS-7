58f595dcdcc58dbb43fd066f9ea55219af9962db
Funibi <139475082+Funibi@users.noreply.github.com>
2024-04-28 12:08:27 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -9,53 +9,54 @@ struct SYM {
   int prior;
 };
 
-template <typename T>
-class TPQueue {
- private:
-  struct list {
-    T name;
-    list* next;
-  };
-  list* head;
-  list* tail;
+template < typename T >
+  class TPQueue {
+    private: struct list {
+      T name;
+      list * next;
+    };
+    list * head;
+    list * tail;
 
-  list* create(const T& c) {
-    list* t = new list;
-    t->name = c;
-    t->next = nullptr;
-    return t;
-  }
+    list * create(const T & c) {
+      list * t = new list;
+      t -> name = c;
+      t -> next = nullptr;
+      return t;
+    }
 
- public:
-  TPQueue() : head(nullptr), tail(nullptr){};
-  void push(const T& c) {
-    list* t = create(c);
-    if (head == nullptr) {
-      head = t;
-      tail = t;
-    } else {
-      if (c.prior > head->name.prior) {
-        t->next = head;
+    public: TPQueue(): head(nullptr),
+    tail(nullptr) {};
+    void push(const T & c) {
+      list * t = create(c);
+      if (head == nullptr) {
         head = t;
+        tail = t;
       } else {
-        list* f = head;
-        while (f->next != nullptr && f->next->name.prior >= c.prior) {
-          f = f->next;
+        if (c.prior > head -> name.prior) {
+          t -> next = head;
+          head = t;
+        } else {
+          list * f = head;
+          while (f -> next != nullptr && f -> next -> name.prior >= c.prior) {
+            f = f -> next;
+          }
+          t -> next = f -> next;
+          f -> next = t;
+          if (t -> next == nullptr) tail = t;
         }
-        t->next = f->next;
-        f->next = t;
-        if (t->next == nullptr) tail = t;
       }
     }
-  }
-  const T pop() {
-    list* t = head;
-    T res = t->name;
-    head = head->next;
-    delete t;
-    return res;
-  }
-  bool isEmpty() const { return head == nullptr; }
-};
+    const T pop() {
+      list * t = head;
+      T res = t -> name;
+      head = head -> next;
+      delete t;
+      return res;
+    }
+    bool isEmpty() const {
+      return head == nullptr;
+    }
+  };
 
-#endif  // INCLUDE_TPQUEUE_H_
+#endif // INCLUDE_TPQUEUE_H_
