93ab51ca62a91629e6797f0ac1dc537cb576133c
kkerwqss <banykinaks@yandex.ru>
2024-04-28 18:25:16 +0300

    update h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,6 +5,58 @@
 template<typename T>
 class TPQueue {
   // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+  struct Item {
+    T data;
+    Item* next;
+    int prior;
+  };
+  Item* head;
+  Item* tail;
+ public:
+  TPQueue(): head(nullptr), tail(nullptr) {}
+  ~TPQueue() {
+    while (head) {
+        Item* temp = head;
+        head = head->next;
+        delete temp;
+    }
+  }
+
+  void push(const SYM& val) {
+    Item* newV = new Item;
+    newV->data = val;
+    newV->next = nullptr;
+    newV->prior = val.prior;
+
+    if (head == nullptr) {
+      head = newV;
+      tail = newV;
+    } else {
+      if (item.prior > head->prior) {
+        temp->next = head;
+        head = newV;
+      } else {
+        Item temp = head;
+        while (temp->next && val.prior <= temp->next->prior) {
+          temp = temp->next;
+        }
+        newV->next = temp->next;
+        temp->next = newV;
+        if (!newV->next) {
+          tail = newV;
+        }
+      }
+    }
+  }
+
+  T pop() {
+    SYM data = head->data;
+    Item* temp = head;
+    head = head->next;
+    delete temp;
+    return data;
+  }
 };
 
 struct SYM {
