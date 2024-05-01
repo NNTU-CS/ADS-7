c93a5b35118eab180bc161e1bc51205edf55ada0
Юлия <160242381+Julia-Achoo@users.noreply.github.com>
2024-05-01 09:49:35 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,44 +5,44 @@
 template<typename T>
 class TPQueue {
  private:
-   struct Item {
-     T item;
-     Item* next;
-   };
-   Item* start;
-   Item* end;
+  struct Item {
+    T item;
+    Item* next;
+  };
+  Item* start;
+  Item* end;
 
  public:
-   TPQueue(): start(nullptr), end(nullptr) {}
-   void push(const T& value) {
-   Item* temp = new Item;
-   temp->item = value;
-   temp->next = nullptr;
-   if (start == nullptr) {
-     start = temp;
-     end = temp;
-     return;
-   }
-   if (start->item.prior < value.prior) {
-     temp->next = start;
-     start = temp;
-     return;
-   }
-     Item* i = start;
-     while (i->next != nullptr && i->next->item.prior >= value.prior)
-       i = i->next;
-       temp->next = i->next;
-       i->next = temp;
-       if (temp->next == nullptr)
-         end = temp;
-       }
-       const T pop() {
-       Item* temp = start;
-       T result = temp->item;
-       start = start->next;
-       delete temp;
-       return result;
-       }
+      TPQueue(): start(nullptr), end(nullptr) {}
+      void push(const T& value) {
+      Item* temp = new Item;
+      temp->item = value;
+      temp->next = nullptr;
+      if (start == nullptr) {
+        start = temp;
+        end = temp;
+        return;
+      }
+      if (start->item.prior < value.prior) {
+        temp->next = start;
+        start = temp;
+        return;
+    }
+      Item* i = start;
+      while (i->next != nullptr && i->next->item.prior >= value.prior)
+        i = i->next;
+      temp->next = i->next;
+      i->next = temp;
+      if (temp->next == nullptr)
+        end = temp;
+    }
+      const T pop() {
+      Item* temp = start;
+      T result = temp->item;
+      start = start->next;
+      delete temp;
+      return result;
+    }
 };
 
 struct SYM {
