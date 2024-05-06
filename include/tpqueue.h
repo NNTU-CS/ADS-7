b96b370513812ef5ec6711363a72b9ad6363d95f
Vio5 <160281229+OsbornEinzbern@users.noreply.github.com>
2024-05-06 14:11:20 +0300

    Upd plug v1.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,9 +2,31 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T>
+template <typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+  T arr[100] = {};
+  int ind = 0, F = 0;
+
+ public:
+  T pop() {
+    F += 1;
+    return arr[F - 1];
+  }
+
+  void push(T elem) {
+    arr[ind] = elem;
+    ind += 1;
+    for (int i = F; i < ind; i++) {
+      for (int j = F; j < ind - 1; j++) {
+        if (arr[j].prior < arr[j + 1].prior) {
+          T boofer = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = boofer;
+        }
+      }
+    }
+  }
 };
 
 struct SYM {
