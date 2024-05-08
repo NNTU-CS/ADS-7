0f0241c9edc297232dc1b3be088deef3f7f6e68f
laal28 <129623228+laal28@users.noreply.github.com>
2024-05-08 22:47:32 +0300

    Solved

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,9 +2,52 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
+#include <stdexcept>
+
+class Node {
+ public:
+    T data;
+    Node* next;
+    Node( T data, Node* next ) : data{data},
+                                 next{next} {};
+};
+
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+    Node* head;
+
+ public:
+    TPQueue() : head(nullptr) {}
+    ~TPQueue() {
+        for (;head;) {
+            pop();
+        }
+    }
+    void push(const T& value) {
+        Node* temp = new Node( value, nullptr );
+        if (!head || value.prior > head->data.prior) {
+            temp->next = head;
+            head = temp;
+        } else {
+            Node* curr = head;
+            for (;curr->next && value.prior <= curr->next->data.prior;) {
+                curr = curr->next;
+            }
+            temp->next = curr->next;
+            curr->next = temp;
+        }
+    }
+    T pop() {
+        if (!head) {
+            throw std::underflow_exception("Queue empty");
+        }
+        T res = head->data;
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+        return res;
+    }
 };
 
 struct SYM {
