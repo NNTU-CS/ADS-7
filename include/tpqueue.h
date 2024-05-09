c62aba37f0ab0c59f3b1528f3ad279f16900f12e
artemchikkey <160394124+artemchikkey@users.noreply.github.com>
2024-05-09 13:25:26 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -6,51 +6,63 @@
 template<typename T>
 class TPQueue {
  private:
-    struct Node {
-        T data;
-        Node* next;
-    };
+  struct El {
+    El* next = nullptr;
+    El* prev = nullptr;
+    T data;
+  };
 
-    Node* head;
+  El* first = nullptr;
+  El* last = nullptr;
 
-    TPQueue() : head(nullptr) {}
+ public:
+  void push(T new_el) {
+    El* el = new El;
+    el->data = new_el;
 
-    public:
-    void push(const T& item) {
-        Node* newNode = new Node;
-        newNode->data = item;
-
-        if (!head || item.prior > head->data.prior) {
-            newNode->next = head;
-            head = newNode;
-        } else {
-            Node* current = head;
-            while (current->next && item.prior <= current->next->data.prior) {
-                current = current->next;
-            }
-            newNode->next = current->next;
-            current->next = newNode;
-        }
+    if (first == nullptr) {
+      first = el;
+      last = el;
+      return;
     }
 
-    T pop() {
-        if (!head) {
-            std::cerr << "Queue is empty!" << std::endl;
-            return T();
-        }
-
-        T item = head->data;
-        Node* temp = head;
-        head = head->next;
-        delete temp;
-
-        return item;
+    El* before_el = last;
+    El* after_el = nullptr;
+    while ((before_el != nullptr) &&
+           (before_el->data.prior < el->data.prior)) {
+      after_el = before_el;
+      before_el = before_el->prev;
     }
+    el->prev = before_el;
+    el->next = after_el;
+
+    if (after_el) {
+      after_el->prev = el;
+    } else {
+      last = el;
+    }
+
+    if (before_el) {
+      before_el->next = el;
+    } else {
+      first = el;
+    }
+  }
+
+  T pop() {
+    T res = first->data;
+    El* after_1 = first->next;
+    if (after_1) {
+      after_1->prev = nullptr;
+    }
+    delete first;
+    first = after_1;
+    return res;
+  }
 };
 
 struct SYM {
   char ch;
   int prior;
 };
-
 #endif  // INCLUDE_TPQUEUE_H_
