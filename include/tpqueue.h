ed4199383d15a821fdafa4072f54c57cec2590dc
AntiCreep753 <160477169+AntiCreep753@users.noreply.github.com>
2024-05-14 10:15:18 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,6 +5,58 @@
 template<typename T>
 class TPQueue {
   // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+  struct elementOfList {
+    T element;
+    elementOfList* nextPointer = nullptr;
+  };
+  elementOfList* head;
+  void insertElementAfter(elementOfList* elemOfList, T elem) {
+    elementOfList* insertingEl = new elementOfList;
+    insertingEl->element = elem;
+    insertingEl->nextPointer = elemOfList->nextPointer;
+    elemOfList->nextPointer = insertingEl;
+  }
+  void insertHead(T elem) {
+  elementOfList* insertingEl = new elementOfList;
+  insertingEl->element = elem;
+  insertingEl->nextPointer = head;
+  head = insertingEl;
+  }
+  void removeHead() {
+  head = head->nextPointer;
+  }
+
+ public:
+  TPQueue() { }
+  void push(T element) {
+    if (head == nullptr) {
+      insertHead(element);
+    } else {
+      elementOfList* currentEl;
+      currentEl = head;
+      if (currentEl->element.prior < element.prior) {
+      insertHead(element);
+      } else {
+        if (currentEl->nextPointer == nullptr) {
+          insertElementAfter(currentEl, element);
+          return;
+        }
+      while (currentEl->nextPointer != nullptr) {
+        if (currentEl->nextPointer->element.prior < element.prior) {
+          insertElementAfter(currentEl, element);
+          break;
+        }
+          currentEl = currentEl->nextPointer;
+        }
+      }
+    }
+  }
+  T pop() {
+    T res = head->element;
+    removeHead();
+    return res;
+  }
 };
 
 struct SYM {
