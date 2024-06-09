3eb0fa5f2978cdddac1862004158a73a2a32697b
Евгений <160422749+MAKARENNA110@users.noreply.github.com>
2024-06-09 18:01:25 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,8 +4,87 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+    struct Item {
+        T data;
+        Item* next;
+    };
+    Item* create(const T&);
+    bool IsEmpty();
+    Item* head;
+    Item* tail;
+
+ public:
+    TPQueue();
+    void rmHead();
+    ~TPQueue();
+    void push(T);
+    T pop();
 };
+template<typename T>
+void TPQueue<T>::rmHead() {
+    if (head && tail) {
+        Item* temp = head->next;
+        T data = head->data;
+        delete head;
+        head = temp;
+        if (!head) {
+            tail = nullptr;
+        }
+    }
+}
+template <typename T>
+typename TPQueue<T>::Item* TPQueue<T>::create(const T& data) {
+    Item* item = new Item;
+    item->data = data;
+    item->next = nullptr;
+    return item;
+}
+template<typename T>
+bool TPQueue<T>::IsEmpty() {
+    return head == nullptr;
+}
+template<typename T>
+TPQueue<T>::TPQueue() {
+    head = tail = nullptr;
+}
+template<typename T>
+TPQueue<T>::~TPQueue() {
+    if (head) {
+        rmHead();
+    }
+}
+template<typename T>
+void TPQueue<T>::push(T sym) {
+    if (IsEmpty()) {
+        head = tail = create(sym);
+    } else {
+        Item* beg = head;
+        Item* temp = create(sym);
+        if (head->data.prior < temp->data.prior) {
+            temp->next = head;
+            head = temp;
+        } else {
+            while (beg->next && beg->next->data.prior >= temp->data.prior) {
+                beg = beg->next;
+            }
+            temp->next = beg->next;
+            beg->next = temp;
+        }
+    }
+}
+template<typename T>
+T TPQueue<T>::pop() {
+    if (head && tail) {
+        Item* temp = head->next;
+        T data = head->data;
+        delete head;
+        head = temp;
+        if (!head) {
+            tail = nullptr;
+        }
+        return data;
+    }
+}
 
 struct SYM {
   char ch;
